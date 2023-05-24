609527f0284209c15111b41675e4b450b451d0b5
Adarsh Verma <adarshverma549@gmail.com>
2023-05-24 14:10:15 +0530

    Add Header comp...

---
+++ myfirstviteapp/src/Components/Header.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+import Navbar from "./Navbar";
+import asset_logo from '../assets/asset_logo.png';
+import { styled } from "styled-components";
+
+// Header Section
+const Header = () => {
+  return (
+    <>
+      <MainHeader>
+        <NavLink to='/'>
+          <img className="logo" src={asset_logo} alt='logo' />
+        </NavLink>
+        <Navbar />
+      </MainHeader>
+    </>
+  );
+};
+export default Header;
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 7rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: auto;
+    max-width: 170px;
+  }
+  // @media (max-width: ${({ theme }) => theme.media.tab}) {
+  //   padding: 0 1.8rem;
+  // }
+`;
